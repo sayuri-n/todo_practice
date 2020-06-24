e2d15252c4d00f93c3f2925955da7201e8847df9
sayuri sannomiya <sayurinokita@gmail.com>
2020-06-24 11:21:54 +0800

    update store function

--- store.php
+++ store.php
@@ -1,13 +1,18 @@
 <?php
 
 // ファイルの読み込み
+require_once('Models/Task.php');
 
-
-// データの受け取り
-
+//フォームからの データの受け取り
+$title = $_POST['title'];
+$contents = $_POST['contents'];
+// /作成した変数
+$currentTime =date("Y/m/d H:i:s");
 
 // DBへのデータ保存
-
+$task = new Task();
+$task->create([$title,$contents,$currentTime]);
 
 // リダイレクト
-
+header('Location: index.php');
+exit;
