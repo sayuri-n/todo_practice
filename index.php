c007a467eeb4cc2e0901667661c7e6340d1b524c
sayuri sannomiya <sayurinokita@gmail.com>
2020-06-25 09:02:52 +0800

    no message

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 // タスクの一覧表示機能
 // ファイルの読み込み
+require_once("Models/Task.php");
+require_once("function.php");
 
 
 // データの取得
-
+$task = new Task();
+$tasks = $task->getAll();
+// var_dump($tasks)
 
 
 ?>
@@ -53,13 +57,14 @@
         </div>
 
         <div class="row p-3">
+            <?php foreach($tasks as $task) : ?>
             <div class="col-sm-6 col-md-4 col-lg-3 py-3 py-3">
                 <div class="card">
                     <img src="https://picsum.photos/200" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">ここにタイトル</h5>
+                        <h5 class="card-title"><?=h($task["title"]); ?></h5>
                         <p class="card-text">
-                            ここに詳細
+                            <?= h($task["contents"]);?>
                         </p>
                         <div class="text-right d-flex justify-content-end">
                             <!-- * href内を変更する -->
@@ -73,6 +78,7 @@
                     </div>
                 </div>
             </div>
+            <?php endforeach;?>
         </div>
     </div>
 
