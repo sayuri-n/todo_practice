4b373201015bfcd9b9c2e48ca793e67761ce67ef
sayuri sannomiya <sayurinokita@gmail.com>
2020-06-26 12:16:16 +0800

    set key

--- dbconnect.php
+++ dbconnect.php
@@ -7,12 +7,18 @@ class DbManager
     public function connect()
     {
         //DBに接続
-        $host = "localhost";
-        // データベース名を変えた場合はこちらを変更する（$dbname = "データベース名"）
-        $dbname = "todo";
-        $charset = "utf8mb4";
-        $user = 'root';
-        $password = '';
+        // $host = "localhost";
+        // // データベース名を変えた場合はこちらを変更する（$dbname = "データベース名"）
+        // $dbname = "todo";
+        // $charset = "utf8mb4";
+        // $user = 'root';
+        // $password = '';
+        $host = getenv('host'); //MySQLがインストールされてるコンピュータ
+        $dbname = getenv('dbname'); //使用するDB
+        $charset = "utf8"; //文字コード
+        $user = getenv('username'); //MySQLにログインするユーザー名
+        $password = getenv('password'); //ユーザーのパスワード
+
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
